9ae1911b247ed12249aaac615f10211b1ebd97e4
Axel Zavier <axelzavier@gmail.com>
2026-06-20 18:36:59 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.muttum.app',
+  appName: 'muttum-app',
+  webDir: 'www'
+};
+
+export default config;
