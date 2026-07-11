dbaa59f8485fdf89440475c7f2d159bdc41575f7
Axel Zavier <axelzavier@gmail.com>
2026-07-11 20:57:26 +0200

    chore(): rename app to muttum

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.muttum.app',
-  appName: 'muttum-app',
+  appName: 'muttum',
   webDir: 'www'
 };
 
