4b1cd00a736bc7955610665dea9daca12d58c628
Axel Zavier <axelzavier@gmail.com>
2026-07-12 20:45:32 +0200

    fix tests

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,3 +1,17 @@
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 
 setupZoneTestEnv();
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
